c88d7f0645ff41d4874f963a8a8b60fb0ee74b78
Arne Rasmussen <Gedesneglen@gmail.com>
2014-12-22 23:33:59 +0100

    I haz commit

---
+++ Black/Black/Project1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("AAABE!");
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
